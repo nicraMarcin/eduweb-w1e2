b87a215d7c878849ff6f916e8f8dd600e6039871
nicraMarcin <marcin@nicram.net>
2016-11-17 19:00:52 +0100

    dodane zadanie domowe js/script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+(function($){
+
+$(document).ready(function(){
+
+	var div = $("div .grid.grid-12")
+	var aHref = $("nav a[href^='http']")
+	var input = $(":radio:not(:checked), :checkbox:not(:checked)")
+	var p = $(":not(span).pagination-item")
+
+})
+
+})(jQuery);
